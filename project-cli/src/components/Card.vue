f037d694b9927af12b599c5d6a7e50bbe90bd513
M4nxx14bbc0 <mparrarodriguez95@gmail.com>
2023-07-07 10:03:45 +0200

    learning how to use vue (laramind) - lesson 8 - Vue CLI init, create project - Vue components - Vue properties - Vue methods

---
+++ project-cli/src/components/Card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="custom-card">
+    <p>{{ text }}</p>
+    <span>{{ subtitle }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['text', 'subtitle']
+}
+</script>
+
+<style scoped>
+.custom-card {
+  height: 150px;
+  width: 150px;
+  background: blue;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.custom-card p {
+  font-size: 20px;
+  text-align: center;
+}
+.custom-card span {
+  font-size: 10px;
+  text-align: center;
+}
+h1, h2{
+  font-weight: normal;
+}
+ul{
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ project-cli/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div class="flex">
+      <app-card v-for="el in listData" :text="el.text" :subtitle="el.subtitle"></app-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      listData: [
+        {text:'Titolo 1', subtitle: 'Sottotitolo 1'},
+        {text:'Titolo 2', subtitle: 'Sottotitolo 2'},
+        {text:'Titolo 3', subtitle: 'Sottotitolo 3'},
+        {text:'Titolo 4', subtitle: 'Sottotitolo 4'},
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.flex {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
